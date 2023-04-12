b41df00d6d878befcb468f82394793343f7a4d60
Ben Nuttle <bennuttle@gmail.com>
2023-04-11 19:59:04 -0700

    about to re-try directory stuff

--- filemanager.py
+++ filemanager.py
@@ -2,6 +2,8 @@ import os
 import music_tag
 import eyed3
 from eyed3.id3.frames import ImageFrame
+import re
+
 
 def music_file_with_missing_attributes(filepath):
     if os.path.splitext(filepath)[1] != '.mp3':
@@ -9,7 +11,6 @@ def music_file_with_missing_attributes(filepath):
 
     file = music_tag.load_file(filepath)
     for key in ["title", "artist", "albumartist", "album", "genre", "year", "tracknumber"]:
-    # for key in ["year"]:
         if not field_is_valid(file[key]):
             return True
     return False
@@ -17,21 +18,21 @@ def music_file_with_missing_attributes(filepath):
 
 def field_is_valid(metadata_item):
     if type(metadata_item.value) == int:
-        #todo this check isn't correct
+        # todo this check isn't correct
         return True
     return len(metadata_item.value) > 0
 
 
-def find_candidate_files(path, filter_fcn=music_file_with_missing_attributes):
+def find_candidate_files(path, quarantine_dir, filter_fcn=music_file_with_missing_attributes):
     for root, dirs, files in os.walk(path):
-        for name in files:
-            full_path = os.path.join(root, name)
-            if filter_fcn(full_path):
-                yield full_path
+        if not root.endswith(quarantine_dir):
+            for name in files:
+                full_path = os.path.join(root, name)
+                if filter_fcn(full_path):
+                    yield full_path
 
 
 def write_file_metadata(filepath, song_details, album_details):
-
     file = music_tag.load_file(filepath)
     file['artist'] = album_details['data'][0]['attributes']['artistName']
     file['albumartist'] = album_details['data'][0]['attributes']['artistName']
@@ -46,7 +47,7 @@ def write_file_metadata(filepath, song_details, album_details):
     # f.tag.year = int(album_details['data'][0]['attributes']['releaseDate'][0:4])
     # f.tag.save()
     # time.sleep(5)
-    #year field is written but only displays when we view it first in the debugger
+    # year field is written but only displays when we view it first in the debugger
     # _ = file['year']
     file.save()
 
@@ -71,8 +72,12 @@ def rename_file(filepath, song_details):
         os.rename(filepath, os.path.join(working_dir, song_details['title'] + '.mp3'))
 
 
-def add_album_artwork(filepath):
+def move_problem_file(filepath, quarantine_dir):
+    base_name = os.path.basename(filepath)
+    os.rename(filepath, os.path.join(quarantine_dir, base_name))
 
+
+def add_album_artwork(filepath):
     base_name = os.path.basename(filepath)
     base_name_no_ext = base_name[:base_name.rindex('.')]
     dir_name = os.path.dirname(filepath)
@@ -100,3 +105,56 @@ def cleanup_jpg_files(working_dir):
         for filename in files:
             if filename.endswith('.jpg'):
                 os.remove(os.path.join(root, filename))
+
+
+def makedir_if_absent(base_path, path):
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except (NotADirectoryError, FileNotFoundError):
+            new_path = base_path
+            relative_path = os.path.relpath(path, base_path)
+            relative_path_dirlist = relative_path.split(os.path.sep)
+            for directory in relative_path_dirlist:
+                new_path = os.path.join(new_path, re.sub(r'[^\w_. -]', '_', directory))
+            if not os.path.exists(new_path):
+                os.makedirs(new_path)
+
+    return path
+
+
+def reorganize_files(path, quarantine_dir):
+    for root, dirs, files in os.walk(path):
+        if not root.endswith(quarantine_dir):
+            for name in files:
+                full_path = os.path.join(root, name)
+                organize_song_by_tags(path, full_path)
+
+
+def organize_song_by_tags(base_music_path, filepath):
+    file = music_tag.load_file(filepath)
+    artist_name = str(file['artist'])
+    album_name = str(file['album'])
+
+    base_name = os.path.basename(filepath)
+    artist_dir = makedir_if_absent(base_music_path, os.path.join(base_music_path, artist_name))
+    album_dir = makedir_if_absent(base_music_path, os.path.join(base_music_path, artist_name, album_name))
+    directory_list = album_dir.split(os.sep)
+    if not check_song_location(directory_list, artist_name, album_name):
+        os.rename(filepath, os.path.join(album_dir, base_name))
+
+
+def check_song_location(directory_list, artist_name, album_name):
+    if len(directory_list) < 2:
+        return False
+
+    if directory_list[-1] != album_name:
+        return False
+
+    if directory_list[-2] != artist_name:
+        return False
+
+    return True
+
+
+def sanitize_name_for_directory(name):
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+import os.path
+
 import pydub.exceptions
 import requests
 
 from shazammanager import ShazamManager
 from audiomanager import bytestream_from_unknown_mp3
-from filemanager import find_candidate_files, write_file_metadata, rename_file, cleanup_raw_files, add_album_artwork, cleanup_jpg_files
+from filemanager import find_candidate_files, write_file_metadata, rename_file, cleanup_raw_files, add_album_artwork, cleanup_jpg_files, move_problem_file, makedir_if_absent, reorganize_files
 
 
-CLEANUP_DIRECTORY = "C:\\Users\\bennu\\OneDrive\\Desktop\\Chelsea_Phone\\test"
+CLEANUP_DIRECTORY = "C:\\Users\\bennu\\OneDrive\\Desktop\\chelsea_phone_test"
+BAD_FILES_DIRNAME = "TuneTagger_BadFiles"
+bad_files_directory = os.path.join(CLEANUP_DIRECTORY, BAD_FILES_DIRNAME)
+makedir_if_absent(CLEANUP_DIRECTORY, bad_files_directory)
 
 sm = ShazamManager()
-for filepath in find_candidate_files(CLEANUP_DIRECTORY):
+for filepath in find_candidate_files(CLEANUP_DIRECTORY, BAD_FILES_DIRNAME):
     try:
         b = bytestream_from_unknown_mp3(filepath)
         song_id = sm.detect_raw_audio(b)['matches'][0]['id']
@@ -23,24 +28,29 @@ for filepath in find_candidate_files(CLEANUP_DIRECTORY):
         rename_file(filepath, song_details)
     except requests.HTTPError:
         print("Bad API response when processing " + filepath + ". Continuing..")
+        move_problem_file(filepath, bad_files_directory)
         continue
     except KeyError:
         print("Incomplete Metadata for " + filepath + ". Continuing..")
+        move_problem_file(filepath, bad_files_directory)
         continue
     except IndexError:
-        #todo why are we getting this when we have good response?
         # we had no matches from shazam API so we get a key error when trying to ID the song on ln 15
         print("??? " + filepath + ". Continuing..")
+        move_problem_file(filepath, bad_files_directory)
         continue
     except FileNotFoundError:
         print("Bad path? " + filepath + ". Continuing..")
+        move_problem_file(filepath, bad_files_directory)
         continue
     except pydub.exceptions.CouldntDecodeError:
         # might be fixed by the code change to from_file??
         print("WTF mate?")
+        move_problem_file(filepath, bad_files_directory)
         continue
 
 cleanup_raw_files()
 cleanup_jpg_files(CLEANUP_DIRECTORY)
+reorganize_files(CLEANUP_DIRECTORY, bad_files_directory)
 
 
